663190249c7ffd6e58b163137a9d3f30708e2e2d
Leandro Martinez <lmartine@unicamp.br>
2024-04-25 10:03:08 -0300

    add doc tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,12 @@
-using TestItemRunner
+using TestItemRunner: @run_package_tests, @testitem
 @run_package_tests
 
 @testitem "Aqua.test_all" begin
     import Aqua
     Aqua.test_all(Packmol)
+end
+
+@testitem "Doctests" begin
+    using Documenter: doctest
+    doctest(Packmol)
 end
