cb60ae2654f3fb797e05d58db30d02debc3c747e
Leandro Martinez <lmartine@unicamp.br>
2024-08-25 18:10:20 -0300

    do not move into folder to run packmol

--- src/packmol_runner.jl
+++ src/packmol_runner.jl
@@ -23,7 +23,6 @@ function run_packmol end
 @doc (@doc run_packmol)
 function run_packmol(input_file::String)
     isfile(input_file) || error("Input file not found: $input_file")
-    cd(dirname(abspath(input_file)))
     # Run packmol  
     run(pipeline(`$packmol_runner`, stdin=input_file))
     println("Wrote output to: ", dirname(abspath(input_file)))
