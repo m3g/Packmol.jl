bc66411e043e2117b038418dcf92deace14ebde1
Leandro Martinez <lmartine@unicamp.br>
2024-08-25 18:35:25 -0300

    fix tests to not cd into dirs

--- src/packmol_runner.jl
+++ src/packmol_runner.jl
@@ -38,10 +38,11 @@ end
 @testitem "run_packmol" begin
     using Packmol
     test_dir = Packmol.src_dir*"/../test/run_packmol"
-    run_packmol("$test_dir/water_box.inp")
-    @test isfile("$test_dir/water_box.pdb")
-    run_packmol("$test_dir/ieee_signaling.inp")
-    @test isfile("$test_dir/ieee_signaling_box.pdb")
+    cd(test_dir)
+    run_packmol("water_box.inp")
+    @test isfile("water_box.pdb")
+    run_packmol("ieee_signaling.inp")
+    @test isfile("ieee_signaling_box.pdb")
     cd(test_dir)
     rm("water_box.pdb")
     run_packmol("water_box.inp")
